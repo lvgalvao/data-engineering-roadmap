c930d119cc32440c39fbb87ff1e0293e32d766db
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-20 01:25:12 -0300

    refactor: adicionar classe ExcelValidatorUI

--- 00-automacao-data-qualiy-excel-etl/src/app.py
+++ 00-automacao-data-qualiy-excel-etl/src/app.py
@@ -1,13 +1,17 @@
-from frontend import show_ui, display_results, set_page_config
+# from frontend import show_ui, display_results, set_page_config
+from frontend import ExcelValidatorUI
 from backend import process_excel
 
 def main():
-    set_page_config()
-    uploaded_file = show_ui()
+    # set_page_config()
+    ui = ExcelValidatorUI("Validador de Schema de Excel")
+    # uploaded_file = show_ui()
+    arquivo_excel_carregado = ui.upload_file()
 
-    if uploaded_file is not None:
-        result, error = process_excel(uploaded_file)
-        display_results(result, error)
+    if arquivo_excel_carregado is not None:
+        result, error = process_excel(arquivo_excel_carregado)
+        # display_results(result, error)
+        ui.display_results(result, error)
 
 if __name__ == "__main__":
     main()
--- 00-automacao-data-qualiy-excel-etl/src/frontend.py
+++ 00-automacao-data-qualiy-excel-etl/src/frontend.py
@@ -1,15 +1,35 @@
 import streamlit as st
 
-def set_page_config():
-    st.set_page_config(page_title="Validador de Schema de Excel", layout="wide")
+class ExcelValidatorUI:
+    def __init__(self, title):
+        self.title = title
+        st.set_page_config(page_title=self.title, layout="wide")
 
-def show_ui():
-    st.title("Validador de Schema de Excel")
-    uploaded_file = st.file_uploader("Carregue seu arquivo Excel aqui", type=["xlsx"])
-    return uploaded_file
+    def display_header(self):
+        st.title(self.title)
 
-def display_results(result, error):
-    if error:
-        st.error(f"Erro na validação: {error}")
-    else:
-        st.success("O schema do arquivo Excel está correto!")
+    def upload_file(self):
+        uploaded_file = st.file_uploader("Carregue seu arquivo Excel aqui", type=["xlsx"])
+        return uploaded_file
+
+    def display_results(self, result, error):
+        if error:
+            st.error(f"Erro na validação: {error}")
+        else:
+            st.success("O schema do arquivo Excel está correto!")
+
+# import streamlit as st
+
+# def set_page_config():
+#     st.set_page_config(page_title="Validador de Schema de Excel", layout="wide")
+
+# def show_ui():
+#     st.title("Validador de Schema de Excel")
+#     uploaded_file = st.file_uploader("Carregue seu arquivo Excel aqui", type=["xlsx"])
+#     return uploaded_file
+
+# def display_results(result, error):
+#     if error:
+#         st.error(f"Erro na validação: {error}")
+#     else:
+#         st.success("O schema do arquivo Excel está correto!")
