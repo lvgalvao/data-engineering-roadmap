f16d03cd72e645e59c9310791b16a9428d560ceb
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-20 01:09:03 -0300

    feat: adicionar check de colunas extras

--- 00-automacao-data-qualiy-excel-etl/src/backend.py
+++ 00-automacao-data-qualiy-excel-etl/src/backend.py
@@ -5,10 +5,21 @@ def process_excel(uploaded_file):
     try:
         df = pd.read_excel(uploaded_file)
 
-        for _, row in df.iterrows():
-            _ = UsuarioSchema(**row.to_dict())
+        # Verifique se há colunas extras no DataFrame
+        extra_cols = set(df.columns) - set(UsuarioSchema.model_fields.keys())
+        if extra_cols:
+            return False, f"Colunas extras detectadas no Excel: {', '.join(extra_cols)}"
 
-        return True, None  # Processamento bem-sucedido
+        # Verifique se há as colunas do schema no DataFrame
+        for index, row in df.iterrows():
+            try:
+                _ = UsuarioSchema(**row.to_dict())
+            except Exception as e:
+                raise ValueError(f"Erro na linha {index + 2}: {e}")  # +2 porque o índice começa em 0 e a primeira linha do Excel é o cabeçalho
 
+        return True, None
+
+    except ValueError as ve:
+        return False, str(ve)
     except Exception as e:
-        return False, str(e)  # Erro durante o processamento
+        return False, f"Erro inesperado: {str(e)}"
