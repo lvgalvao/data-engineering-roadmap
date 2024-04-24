70fed1e68dd2f021b86abbaef99e902050b38e1a
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-04-24 11:53:21 -0300

    desafios

---
+++ Bootcamp - SQL e Analytics/Aula-03/desafio.sql
@@ -0,0 +1,13 @@
+-- 1. Cria um relatório para todos os pedidos de 1996 e seus clientes (152 linhas)
+
+-- 2. Cria um relatório que mostra o número de funcionários e clientes de cada cidade que tem funcionários (5 linhas)
+
+-- 3. Cria um relatório que mostra o número de funcionários e clientes de cada cidade que tem clientes (69 linhas)
+
+-- 4.Cria um relatório que mostra o número de funcionários e clientes de cada cidade (71 linhas)
+
+-- 5. Cria um relatório que mostra a quantidade total de produtos encomendados.
+-- Mostra apenas registros para produtos para os quais a quantidade encomendada é menor que 200 (5 linhas)
+
+-- 6. Cria um relatório que mostra o total de pedidos por cliente desde 31 de dezembro de 1996.
+-- O relatório deve retornar apenas linhas para as quais o total de pedidos é maior que 15 (5 linhas)
