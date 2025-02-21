8b7e213224fdc5c8db49c833474eb84a5d90dfed
Luciano Vasconcelos <lvgalvaofilho@gmail.com>
2025-02-21 10:29:00 -0300

    tabelas

---
+++ Bootcamp - SQL e Analytics/Aula-Databricks/tabela_06.sql
@@ -0,0 +1,4 @@
+INSERT INTO region VALUES (1, 'Eastern');
+INSERT INTO region VALUES (2, 'Western');
+INSERT INTO region VALUES (3, 'Northern');
+INSERT INTO region VALUES (4, 'Southern');
---
+++ Bootcamp - SQL e Analytics/Aula-Databricks/tabela_08.sql
@@ -0,0 +1,6 @@
+INSERT INTO shippers VALUES (1, 'Speedy Express', '(503) 555-9831');
+INSERT INTO shippers VALUES (2, 'United Package', '(503) 555-3199');
+INSERT INTO shippers VALUES (3, 'Federal Shipping', '(503) 555-9931');
+INSERT INTO shippers VALUES (4, 'Alliance Shippers', '1-800-222-0451');
+INSERT INTO shippers VALUES (5, 'UPS', '1-800-782-7892');
+INSERT INTO shippers VALUES (6, 'DHL', '1-800-225-5345');
